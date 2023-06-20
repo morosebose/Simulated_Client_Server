f5e094c18a3418f87e59b203a732c88251d687cf
Surajit Bose <verbose@duck.com>
2023-06-20 02:55:11 -0700

    Upload working version of code

---
+++ client.py
@@ -0,0 +1,118 @@
+'''
+CIS 41B Spring 2023
+Surajit A. Bose
+Lab 5 Client
+'''
+
+import socket
+import pickle
+
+# Constants
+HOST = '127.0.0.1'  # IP address of client
+PORT = 5113         # Port at which to connect to server
+
+def changeResult (recd) :
+    '''
+    Handle result of request to change directory where client currently is
+    
+    @param recd tuple with path to old dir and:
+        - if success, path to new dir
+        - if failure, boolean False
+    @return cur_dir path to dir where client currently is
+    '''
+    if recd[1]:
+        cur_dir = recd[1]
+        print('New path: ' + cur_dir)
+    else:
+        cur_dir = recd[0]
+        print ('Invalid path')
+    return cur_dir
+
+
+def listResult (recd) :
+    '''
+    Handle result of request for listing of directory where client currently is
+    
+    @param recd tuple with path to current dir and listing of dir
+    @return cur_dir path to dir where client currently is
+    '''
+    cur_dir = recd[0]
+    if recd[1] :
+        print(f'Directories and files found under {cur_dir} :\n-', end = ' ')
+        print('\n- '.join(recd[1]))
+    else :
+        print('Empty directory')
+    return cur_dir
+
+
+def createResult (recd) : 
+    '''
+    Handle result of request to create new file in dir where client is
+    
+    @param recd tuple with path to current dir and Boolean indicating success
+    @return cur_dir path to dir where client currently is
+    '''
+    cur_dir = recd[0]
+    if recd[1] :
+        print(f'File created in {cur_dir}')
+    else :
+        print('File already exists')
+    return cur_dir
+        
+
+def handleConnection(s, cur_dir) :
+    '''
+    Get and validate requests from user, and handle communication with server
+    @param s socket at which server is connected
+    @param cur_dir path to dir where client currently is
+    @return None
+    '''
+    menu = '''
+        c. change directory
+        l. list directory
+        f. create file
+        q. quit
+        '''
+        
+    menu_dict =  {'c': changeResult, 'l' : listResult, 'f' : createResult}
+    
+    print(menu)
+    choice = input('Enter choice: ').lower()
+    
+    while choice != 'q' :
+        if choice not in menu_dict :
+            print('Invalid choice, please try again.')       
+        else :
+            to_send = []
+            to_send.append(choice)
+            to_send.append(cur_dir)
+            if choice == 'c':
+                to_send.append(input('Enter path, starting from current directory: '))
+            elif choice == 'f' :
+                to_send.append(input('Enter filename: '))
+            s.send(pickle.dumps(to_send))
+            recd = pickle.loads(s.recv(1024))
+            cur_dir = menu_dict[choice](recd)
+        print(menu)
+        choice = input('Enter choice: ').lower()
+    s.send(pickle.dumps([choice]))
+
+
+def main() :
+    '''
+    Code driver to connect to server and get current directory on server
+
+    @return None
+    '''
+    with socket.socket() as s :
+        s.connect((HOST, PORT))
+        print("Client connect to:", HOST, "port:", PORT)
+        s.send(pickle.dumps(['g']))
+        cur_dir = pickle.loads(s.recv(1024))
+        handleConnection(s, cur_dir)
+    
+        print('Closed connection to server')
+        
+
+if __name__ == '__main__' :
+    main()
---
+++ server.py
@@ -0,0 +1,166 @@
+'''
+CIS 41B Spring 2023
+Surajit A. Bose
+Lab 5 Server
+'''
+
+import socket
+import os
+import pickle
+import threading
+import sys
+
+# Constants
+HOST = 'localhost'  # Server hostname
+PORT = 5113         # Port at which server is listening
+MIN_CLIENTS = 1     # Minimum number of clients   
+MAX_CLIENTS = 4     # Maximum number of clients 
+MIN_TIMEOUT = 3     # Minimum connection timeout in seconds
+MAX_TIMEOUT = 30    # Maximum connection timeout in seconds
+
+    
+def changeDir(old_path, new_path) :
+    '''
+    Change the current dir where client is on the server
+    
+    @param old_path the current dir where the client is
+    @param new_path the path to the dir to which the client wants to move    
+    @return :
+        - if success, tuple with old dir and new dir
+        - if failure, tuple with old dir and False
+    '''
+    try :
+        os.chdir(old_path)
+        os.chdir(new_path)
+        return old_path, os.getcwd()
+    except OSError :
+        return old_path, False
+
+
+def listDir(cur_path) :
+    '''
+    Provide listing of current dir where client is on the server
+    
+    @param cur_path path where the client currently is on the server
+    @return tuple with current path and directory listing at that path
+    '''
+    return cur_path, os.listdir(cur_path)
+
+
+def createFile(cur_path, filename) :
+    '''
+    Create file in current dir where client is on the server, unless 
+    file already exists
+
+    @param cur_path path where the client currently is on the server
+    @return tuple with cur_path and a Boolean to indicate success or failure
+    '''
+    os.chdir(cur_path)
+    success = True
+    try: 
+        if os.path.isfile(filename) :
+            success = False
+        else :
+            f = open(filename, 'a')
+            f.close()
+    except OSError:
+        success = False
+    return cur_path, success
+
+
+def handleConnection(conn, addr, root_dir, name) :
+    '''
+    Get and respond to client requests
+    
+    @param conn the socket to which client is connected
+    @param address tuple with the address of the client and the socket
+    @param name name of the thread in which the connection is made
+    @param root_dir the directory where the server and client start out    
+    @return None
+    '''
+    print(f'{name} connection to client at port: {PORT}, address: {addr}')
+    fun_dict = {'c': changeDir, 'l' : listDir, 'f' : createFile}
+    from_client = pickle.loads(conn.recv(1024))
+    fun = from_client[0]
+
+    while fun != 'q' :
+        if fun == 'g' :
+            mesg = root_dir
+            print(f'Sending {mesg}')
+        else :
+            mesg = fun_dict[fun](*from_client[1:])
+        conn.send(pickle.dumps(mesg))
+        from_client = pickle.loads(conn.recv(1024))
+        fun = from_client[0]
+    print(f'{name} connection closed by client')
+    
+
+def checkArgs(arg_list) :
+    '''
+    Validate arguments sent in on command line
+
+    @return:
+        - if number and type of arguments is correct, empty string
+        - if error, string specifying the error        
+    '''
+    invalid = ''
+    try :
+        if len(arg_list) > 3 :
+            raise RuntimeError
+        num = int(arg_list[1])
+        timeout = int(arg_list[2])
+        if not MIN_CLIENTS <= num <= MAX_CLIENTS :
+            invalid = 'Number of clients must be between 1 and 4'
+        elif not MIN_TIMEOUT <= timeout < MAX_TIMEOUT :
+            invalid = 'Timeout must be between 3 and 30 seconds'
+    except RuntimeError :
+        invalid = 'Received too many command line arguments'
+    except IndexError :
+        invalid = 'Received insufficient number of command line arguments'
+    except ValueError :
+        invalid = 'Unexpected argument in command line, expected integer'
+    return invalid
+    
+    
+def main() :
+    '''
+    Code driver to start server, manage connections, and handle requests
+
+    Raises SystemExit if arguments passed in on command line are invalid
+    
+    @return None
+    '''
+    invalid = checkArgs(sys.argv)
+    if invalid :
+        print('\tUsage: server.py number_of_clients timeout_in_seconds')
+        print(f'\t{invalid}')
+        raise SystemExit('\tPlease check command line arguments and try again')
+    
+    with socket.socket() as to_me :
+        root_dir = os.getcwd()
+        to_me.bind((HOST, PORT))
+        print("Server is up, hostname:", HOST, "port:", PORT)
+    
+        to_me.listen()
+        threads = []
+        names = ['First', 'Second', 'Third', 'Fourth']
+        
+        for i in range (int(sys.argv[1])) :
+            try :
+                cur_name = names[i]
+                to_me.settimeout(int(sys.argv[2]))
+                (conn, addr) = to_me.accept()
+                t = threading.Thread(target = handleConnection, args = (conn, addr, root_dir, cur_name), name = cur_name)
+                threads.append(t)
+                t.start()
+            except socket.timeout :
+                print(f'{cur_name} connection timed out')
+        
+        for t in threads: 
+            t.join()
+    
+        print('No open connections, shutting down server')
+    
+
+if __name__ == '__main__' :
+    main()
